07a1b5aa919db1a4a083c880aa9b177c073d77dc
Fearless-Badger <micah.najacht@gmail.com>
2025-01-28 00:18:22 -0600

    We got filtering happening 😎

--- Filter.java
+++ Filter.java
@@ -26,8 +26,21 @@ public class Filter
       int spacing = 10; // Default number of spaces between the ones places of two adjacent integers.
       int groups = 0;   // Default size of a group. Zero means no groups.
 
+      // READ in the properties data, override the default values - filter.properties
 
+      // READ in the environment values, from the env
+      // names = CS336_COLUMNS or CS336_SPACING or CS336_GROUPS
+      // convert string value to int using "parseInt()"
 
+      // READ the command line args, override env. Use parseInt() again
+
+      /* Your program should read the sequence of input integers from standard input by using the Scanner class methods hasNextLong() and nextLong(). */
+
+      while (in.hasNextInt()) {
+         System.out.println(in.nextInt() + "!");
+      }
+
+      in.close();
 
    }
 }
